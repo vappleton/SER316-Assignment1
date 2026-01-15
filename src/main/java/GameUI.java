187e70bbd377e56f74a48a2d215c349b11a4b2f0
vappleton <139707137+vappleton@users.noreply.github.com>
2026-01-14 22:28:13 -0800

    fix inifnite loop in gameUI

--- src/main/java/GameUI.java
+++ src/main/java/GameUI.java
@@ -12,7 +12,6 @@ public class GameUI {
     public void start() {
         while (!engine.isGameWon() && !engine.hasUserQuit()) {
             System.out.print("Guess a number between " + engine.getMin() + " and " + engine.getMax() + " (or negative to exit): ");
-        }
         while (!engine.isGameWon() && !engine.isGameOver()) {
             System.out.print("Guess a number between " + engine.getMin() + " and " + engine.getMax() + ": ");
             int guess = Utils.readInt(scanner);
@@ -20,5 +19,6 @@ public class GameUI {
             GuessResult result = engine.makeGuess(guess);
             System.out.println(result.getMessage());
         }
+    }
     }
 }
